41e2781e3e0b67013667cc48f96bb497e302ddaf
Prathmesh <pratmbr@gmail.com>
2023-06-27 22:33:53 +0530

    Update users.js

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -18,7 +18,7 @@ export const users = [
     bookmarks: [],
     bio: "It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own. -Marcus Aurelius",
     bannerImg:
-      "https://images.unsplash.com/photo-1509773896068-7fd415d91e2e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80",
+      "https://i.pinimg.com/originals/2e/59/a4/2e59a45889aa828e0e0da51691971655.jpg",
     followers: [
       {
         _id: uuid(),
