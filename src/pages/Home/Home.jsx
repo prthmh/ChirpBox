983d91f43a355d2e6ad5f1a1ac0e5d6bf9c18da3
Prathmesh <pratmbr@gmail.com>
2023-07-10 13:20:42 +0530

    image added in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import chripbox_logo_2  from '../../assets/chripbox_logo_2.png';
+import chripbox_logo_2 from "../../assets/chripbox_logo_2.png";
+import home_img from "../../assets/home_img.svg";
 
 const Home = () => {
   return (
     <section className="home">
+      <div>
+        <img src={home_img} alt="pic" className="home_img" />
+      </div>
       <div className="home_page">
         <img src={chripbox_logo_2} alt="logo" />
-        <h1 className="home_page_heading" > Welcome to ChirpBox</h1>
+        <h1 className="home_page_heading"> Welcome to ChirpBox</h1>
         <Link to="/signup">
-          <button className="join_btn" >Join Now</button>
+          <button className="join_btn">Join Now</button>
         </Link>
-        <br/>
+        <br />
         <Link to="/login">
-          <button className="acc_btn" >Already Have an acc</button>
+          <button className="acc_btn">Already Have an acc</button>
         </Link>
       </div>
     </section>
